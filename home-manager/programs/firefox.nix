7d63849b3efad4c9af0f0072ebf74ca2674a5e6e
lomenzel <leonderdunna@gmail.com>
2024-05-13 19:38:16 +0200

    hide tab bar

--- home-manager/programs/firefox.nix
+++ home-manager/programs/firefox.nix
@@ -6,6 +6,21 @@
       enable = true;
       package = firefox-esr;
       
+      profiles."default" = {
+        userChrome = ''
+            /* Hide tab bar in FF Quantum */
+            @-moz-document url("chrome://browser/content/browser.xul") {
+                #TabsToolbar {
+                visibility: collapse !important;
+                margin-bottom: 21px !important;
+                }
+            
+                #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+                visibility: collapse !important;
+                }
+            }
+        '';
+      };
 
       # Check about:policies#documentation for options.
       policies = {
