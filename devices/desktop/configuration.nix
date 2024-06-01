93c4a4fe5b5cc24cf58c65a539641ff0c0671f06
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-06-01 15:18:13 +0000

    ok

--- devices/desktop/configuration.nix
+++ devices/desktop/configuration.nix
@@ -62,7 +62,7 @@
 
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   # Enable the X11 windowing system.
