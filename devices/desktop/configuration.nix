7d026ebee80140d39aebe9d480da96fb92fa6643
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-06-29 17:46:37 +0200

    deprication

--- devices/desktop/configuration.nix
+++ devices/desktop/configuration.nix
@@ -45,8 +45,8 @@
 
   hardware.opengl = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+  # driSupport = true;
+  # driSupport32Bit = true;
   };
 
   # Load nvidia driver for Xorg and Wayland
