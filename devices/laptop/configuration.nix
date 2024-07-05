b8fc6049d64f54a8ac95803276e33d9d5de23430
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-07-05 10:32:29 +0000

    git credentials store

--- devices/laptop/configuration.nix
+++ devices/laptop/configuration.nix
@@ -14,13 +14,21 @@
 
   services.kubo = {
     enable = true;
-    autoMount = true;
+    autoMount = false;
     localDiscovery = true;
     enableGC = true;
     settings = {
+      API.HTTPHeaders.Access-Control-Allow-Origin = [ "*" ];
+      API.HTTPHeaders.Access-Control-Allow-Methods = [
+        "GET"
+        "POST"
+        "PUT"
+      ];
       Datastore.StorageMax = "200GB";
       Datastore.GCPeriod = "2h";
       Addresses.API = "/ip4/127.0.0.1/tcp/8082";
+      Addresses.Gateway = "/ip4/0.0.0.0/tcp/8081";
+
     };
   };
 
--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -4,9 +4,11 @@
     enable = true;
     userName = "Leonard-Orlando Jonathan Menzel";
     userEmail = "leonard-orlando.jonathan.menzel@stud.th-luebeck.de";
-    extraConfig = ''
-      [save]
-        directory = *
-    '';
+    extraConfig = {
+      save.directory = "*";
+      core.editor = "${pkgs.vim}/bin/vim";
+      pull.rebase = true;
+      credential.helper = "store";
+    };
   };
 }
