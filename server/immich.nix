cf37c92c13e50668bf8441673dfb971e635e274b
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-09-25 11:23:52 +0000

    ???

--- server/immich.nix
+++ server/immich.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ...}: {
   services.immich = {
     enable = true;
-    host = "photos.menzel.lol";
+    host = "localhost";
     port = 8097;
   };
   services.nginx.virtualHosts."photos.menzel.lol" = {
     forceSSL = true;
     useACMEHost = "wildcard";
     locations."/" = {
-      proxyPass = "http://192.168.178.61:8097";
+      proxyPass = "http://localhost:8097";
       proxyWebsockets = true;
     };
   };
