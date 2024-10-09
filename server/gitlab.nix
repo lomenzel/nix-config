18081f126ff4a0d7b8494e051788e624bc93939a
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-10-09 12:32:05 +0000

    concurrent limit

--- server/gitlab.nix
+++ server/gitlab.nix
@@ -24,55 +24,61 @@
   virtualisation.docker.enable = true;
   services.gitlab-runner.enable = true;
 
-  services.gitlab-runner.services = {
-    default = {
-      authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFile;
-      dockerImage = "alpine";
-    };
-    default-1 = {
-      authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFile1;
-      dockerImage = "alpine";
-    };
-      default-2 = {
-      authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFile2;
-      dockerImage = "alpine";
-    };
-      default-3 = {
-      authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFile3;
-      dockerImage = "alpine";
-    };
-    nix = with lib;{
-      authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFileNix;
-      dockerImage = "alpine";
-      dockerVolumes = [
-        "/nix/store:/nix/store:ro"
-        "/nix/var/nix/db:/nix/var/nix/db:ro"
-        "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
-      ];
-      dockerDisableCache = true;
-      preBuildScript = pkgs.writeScript "setup-container" ''
-        mkdir -p -m 0755 /nix/var/log/nix/drvs
-        mkdir -p -m 0755 /nix/var/nix/gcroots
-        mkdir -p -m 0755 /nix/var/nix/profiles
-        mkdir -p -m 0755 /nix/var/nix/temproots
-        mkdir -p -m 0755 /nix/var/nix/userpool
-        mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
-        mkdir -p -m 1777 /nix/var/nix/profiles/per-user
-        mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
-        mkdir -p -m 0700 "$HOME/.nix-defexpr"
-        . ${pkgs.nix}/etc/profile.d/nix-daemon.sh
-        ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-unstable nixpkgs # 3
-        ${pkgs.nix}/bin/nix-channel --update nixpkgs
-        ${pkgs.nix}/bin/nix-env -i ${concatStringsSep " " (with pkgs; [ nix cacert git openssh ])}
-      '';
-      environmentVariables = {
-        ENV = "/etc/profile";
-        USER = "root";
-        NIX_REMOTE = "daemon";
-        PATH = "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
-        NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
+
+  services.gitlab-runner = {
+    settings = {
+      concurrent = 100;
+     };
+    services = {
+      default = {
+        authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFile;
+        dockerImage = "alpine";
       };
-      tagList = [ "nix" ];
+      default-1 = {
+        authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFile1;
+        dockerImage = "alpine";
+      };
+      default-2 = {
+        authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFile2;
+        dockerImage = "alpine";
+      };
+      default-3 = {
+        authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFile3;
+        dockerImage = "alpine";
+      };
+      nix = with lib;{
+        authenticationTokenConfigFile = pkgs.writeText "auth-file" secrets.gitlab.authenticationFileNix;
+        dockerImage = "alpine";
+        dockerVolumes = [
+          "/nix/store:/nix/store:ro"
+          "/nix/var/nix/db:/nix/var/nix/db:ro"
+          "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
+        ];
+        dockerDisableCache = true;
+        preBuildScript = pkgs.writeScript "setup-container" ''
+          mkdir -p -m 0755 /nix/var/log/nix/drvs
+          mkdir -p -m 0755 /nix/var/nix/gcroots
+          mkdir -p -m 0755 /nix/var/nix/profiles
+          mkdir -p -m 0755 /nix/var/nix/temproots
+          mkdir -p -m 0755 /nix/var/nix/userpool
+          mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
+          mkdir -p -m 1777 /nix/var/nix/profiles/per-user
+          mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
+          mkdir -p -m 0700 "$HOME/.nix-defexpr"
+          . ${pkgs.nix}/etc/profile.d/nix-daemon.sh
+          ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-unstable nixpkgs # 3
+          ${pkgs.nix}/bin/nix-channel --update nixpkgs
+          ${pkgs.nix}/bin/nix-env -i ${concatStringsSep " " (with pkgs; [ nix cacert git openssh ])}
+        '';
+        environmentVariables = {
+          ENV = "/etc/profile";
+          USER = "root";
+          NIX_REMOTE = "daemon";
+          PATH = "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
+          NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
+        };
+        tagList = [ "nix" ];
+      };
     };
   };
 
