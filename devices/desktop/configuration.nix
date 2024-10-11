285d6f67723ed47befce28cb0cfedf37623067ca
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-10-11 14:42:04 +0000

    enable docker nvidia

--- devices/desktop/configuration.nix
+++ devices/desktop/configuration.nix
@@ -63,7 +63,7 @@ in
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
-
+  hardware.nvidia-container-toolkit.enable = true;
   hardware.nvidia = {
 
     # Modesetting is required.
