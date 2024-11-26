f4bf81424dd73304e2ac9be83e4a873a6cf2dafa
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-11-26 21:53:44 +0000

    ups

--- server/nginx.nix
+++ server/nginx.nix
@@ -11,7 +11,7 @@
       forceSSL = true;
       useACMEHost = "beta-wildcard";
       locations."/" = {
-        proxyPass = "https://192.168.178.169$request_uri"
+        proxyPass = "https://192.168.178.169$request_uri";
           extraConfig = ''
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
