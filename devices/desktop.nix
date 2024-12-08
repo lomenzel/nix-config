b30475ca36cb9f0b99be86e4262ce33a0cde8988
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-12-08 09:57:01 +0000

    stuff

--- devices/desktop.nix
+++ devices/desktop.nix
@@ -52,17 +52,11 @@
   ];
 
   programs.nix-ld.enable = true;
-
   programs.nix-ld.libraries = with pkgs; [
-
-    # Add any missing dynamic libraries for unpackaged programs
-
-    # here, NOT in environment.systemPackages
     jdk
     rustup
     cargo
     rustc
-
   ];
 
   /*
--- devices/laptop.nix
+++ devices/laptop.nix
@@ -38,52 +38,6 @@
   services.openssh.settings.PermitRootLogin = "yes";
 
   programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    # Add any missing dynamic libraries for unpackaged programs
-    # here, NOT in environment.systemPackages
-
-    ocl-icd
-    stdenv.cc.cc
-    intel-ocl
-    p7zip
-    alsa-lib
-    fpm
-    gcc
-    libgcc
-    nss.out
-    nss
-    dbus
-    atkmm
-    atkmm.out
-    dbus.out
-    dbus-glib
-    dbus-glib.out
-    nspr
-    nspr.out
-    glib
-    electron
-    mesa
-    at-spi2-atk
-    xorg.libX11
-    xorg.libXcomposite
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXrandr
-    xorg.libxcb
-    libxkbcommon
-    expat
-    glibc
-    libdrm
-    cups
-    pango
-    cairo
-    gtk4
-    gtk3
-    glib.out
-    libgccjit
-    stdenv.cc.cc.lib
-    libstdcxx5
-  ];
+  programs.nix-ld.libraries = with pkgs; [];
 
 }
