7e390f6f8ce8c14bd0bfa6ab7343a794b5b7f09b
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-12-29 08:58:22 +0000

    no more droidcam :(

--- devices/desktop.nix
+++ devices/desktop.nix
@@ -50,13 +50,13 @@
     helix
     rsync
   ];
-  programs.droidcam.enable = true;
-   boot.extraModulePackages = with config.boot.kernelPackages; [
-    #v4l2loopback
-  ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  '';
+ # programs.droidcam.enable = true;
+ #  boot.extraModulePackages = with config.boot.kernelPackages; [
+ #    v4l2loopback
+ # ];
+ # boot.extraModprobeConfig = ''
+ #   options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+ # '';
 
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
