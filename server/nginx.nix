4bc83989518fc8e89d2a8d01ea7262ba24f93d9b
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-02-02 17:56:31 +0000

    nginx

--- server/nginx.nix
+++ server/nginx.nix
@@ -6,19 +6,6 @@
     recommendedTlsSettings = true;
     recommendedOptimisation = true;
     recommendedGzipSettings = true;
-
-    virtualHosts."*.beta.menzel.lol" = {
-      forceSSL = true;
-      useACMEHost = "beta-wildcard";
-      locations."/" = {
-        proxyPass = "https://192.168.178.169$request_uri";
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        '';
-      };
-    };
   };
 }
 
