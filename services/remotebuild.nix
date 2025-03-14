ec4568db407aa7f5dff19bd87085501178292439
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-03-14 16:15:53 +0000

    generalize remotebuild

--- services/remotebuild.nix
+++ services/remotebuild.nix
@@ -1,17 +1,16 @@
-{...}: {
+{config,...}: {
   users.users.remotebuild = {
     isNormalUser = true;
     createHome = false;
     group = "remotebuild";
     openssh.authorizedKeys.keyFiles = [
-      ./remotebuild.pub
       ./remotebuild-minive.pub
-    ];
+    ] ++ (if config.networking.hostName != "laptop" then [ ./remotebuild-laptop.pub ] else []);
   };
   users.groups.remotebuild = {};
   nix.settings = {
     trusted-users = [ "remotebuild" ];
-    max-jobs = 20;
+    max-jobs = "auto";
     cores = 0;
     # substituters = [ "https://cache.menzel.lol" ];
     # trusted-public-keys = ["cache.menzel.lol:9HvL7GP4GKds1IiTJxRIRi63lOXixzcikeP9beSDrNk="];
