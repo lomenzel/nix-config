749820b9968fc5ec2457fff14900ecbbab02767a
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-04-01 12:48:24 +0000

    remotebuilders do not emulate different systems

--- services/remotebuild.nix
+++ services/remotebuild.nix
@@ -16,5 +16,5 @@
     # substituters = [ "https://cache.menzel.lol" ];
     # trusted-public-keys = ["cache.menzel.lol:9HvL7GP4GKds1IiTJxRIRi63lOXixzcikeP9beSDrNk="];
   };
-  boot.binfmt.emulatedSystems = [ "armv7l-linux" ];
+  #boot.binfmt.emulatedSystems = [ "armv7l-linux" ];
 }
