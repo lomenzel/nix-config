aa8b7d90cb068dc2757be19bd1fdd74a7b6693c8
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2026-02-10 09:03:26 +0000

    mini

--- devices/mini/home.nix
+++ devices/mini/home.nix
@@ -7,6 +7,8 @@
 {
   home.packages = with inputs.nixpkgs-unstable.legacyPackages."x86_64-linux".pkgsCross.armv7l-hf-multiplatform; [
     pkgs.fractal
+    tuba
+    passes
   ];
   home.username = "leonard";
   home.homeDirectory = "/home/leonard";
