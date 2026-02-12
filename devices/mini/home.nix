ab562f8902a0dcfc24d01520bdecdaa8f8f111b0
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2026-02-12 11:45:17 +0000

    zsh config

--- devices/mini/home.nix
+++ devices/mini/home.nix
@@ -39,11 +39,26 @@
     #nh # broken
     fractal
     gnome-keyring
+    #firefox-mobile
     #tuba # deno needs to build rusty_v8 from source for this to work
     passes # broken (probably some native-buildinputs vs buildinputs issue)
 
   ];
-  programs.zsh.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "direnv"
+      ];
+      theme = "jispwoso";
+    };
+  };
+  targets.genericLinux.enable = true;
+  targets.genericLinux.gpu.enable = false;
+  xdg.enable = true;
   services.gnome-keyring.enable = true;
   home.username = "leonard";
   home.homeDirectory = "/home/leonard";
