f22e870c8ad901bdda7d902d8ef2d3543a17dde6
Leonard-Orlando Jonathan Menzel <leonard@menzel.lol>
2026-09-20 20:23:45 +0200

    contextsize

--- home-manager/programs/opencode.nix
+++ home-manager/programs/opencode.nix
@@ -18,23 +18,23 @@
           "qwen3.8-flash-next:125b-a6b-q4_K_M" = {
             name = "Qwen 3.8 Flash Next";
             limit = {
-              context = 65538;
-              output = 8192;
+              context = 32768;
+              output = 4096;
             };
           };
           "olmo-3.1:32b" = {
             name = "Olmo 3.1";
             limit = {
-              context = 65536;
-              output = 8192;
+              context = 32768;
+              output = 4096;
             };
           };
 
           "qwen3.8:27b" = {
             name = "Qwen 3.8";
             limit = {
-              context = 65536;
-              output = 8192;
+              context = 32768;
+              output = 4096;
             };
             variants = {
               low = {reasoningEffort = "low";};
